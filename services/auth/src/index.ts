8ff7f3a28e0d1dff5dfdcc81d3fd677112ee2939
Avinash Saxena <avinashs1995@gmail.com>
2026-02-14 20:24:24 -0800

    Added basic auth

--- services/auth/src/index.ts
+++ services/auth/src/index.ts
@@ -1,10 +1,13 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import connectDB from './config/db.js';
+import connectDB from './config/db.js'
+import authRoute from './routes/auth.js'
 
-dotenv.config()
+dotenv.config();
 
-const app=express()
+const app=express();
+app.use(express.json());
+app.use("/api/auth", authRoute);
 const PORT = process.env.PORT || 5000;
 
 
